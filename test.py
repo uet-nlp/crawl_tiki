4a79cfba66b61d01e5ded9d1ffde2e8c9bd562e5
Nguyen Manh Cuong <uet.nlp@gmail.com>
2021-06-24 23:58:52 +0700

    ngu

---
+++ test.py
@@ -0,0 +1,9 @@
+class objects:
+    def __init__(self):
+        self.colour = None
+        self.__shape = "Circle" 
+ 
+    def display(self, s):
+        self._shape = s
+obj=objects()
+print(obj._objects__shape)
